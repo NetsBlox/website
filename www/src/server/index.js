92670a8bf45386621870c6e0d3f568269a4d22fe
hamidzr <dellydela@gmail.com>
2017-05-01 12:25:25 -0500

    modularized server views ( pug templates ) huge cleanup up leftovers from webgme

--- www/src/server/index.js
+++ www/src/server/index.js
@@ -1,16 +1,14 @@
-/* Copyright G. Hemingway, @2017 */
 'use strict';
-
 const path            = require('path'),
-    express         = require('express'),
-    bodyParser      = require('body-parser'),
-    axios				= require('axios'),
-    cookieParser    = require('cookie-parser'),
-    logger          = require('morgan');
+		express         = require('express'),
+		bodyParser      = require('body-parser'),
+		axios				= require('axios'),
+		cookieParser    = require('cookie-parser'),
+		logger          = require('morgan');
 
-let port = process.env.PORT ? process.env.PORT : 8000;
-let env = process.env.NODE_ENV ? process.env.NODE_ENV : 'dev';
-let serverAdr = process.env.EDITOR_ADDRESS;
+const PORT = process.env.PORT ? process.env.PORT : 8000;
+const NODE_ENV = process.env.NODE_ENV ? process.env.NODE_ENV : 'dev';
+const SERVER_ADDRESS = process.env.EDITOR_ADDRESS;
 
 /**********************************************************************************************************/
 
@@ -19,7 +17,7 @@ let app = express();
 // parse cookies
 app.use(cookieParser());
 // Setup pipeline logging
-if (env !== 'test') app.use(logger('dev'));
+if (NODE_ENV !== 'test') app.use(logger('dev'));
 // Setup pipeline support for static pages
 app.use(express.static(path.join(__dirname, '../../public')));
 // Setup pipeline support for server-side templates
@@ -31,70 +29,47 @@ app.locals.env = process.env;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// // Import our API Routes
-// require('./api/v1/game')(app);
-
 /**********************************************************************************************************/
 
+app.get('/', (req, res) => {
+		// get the exmaples and public projects data
+		// get examples data
+		let examplesPromise = axios({
+				url: SERVER_ADDRESS + '/api/Examples/EXAMPLES?metadata=true',
+				method: 'get',
+		});
+
+		// get projects data
+		let publicProjectsPromise = axios({
+			method: 'GET',
+			url: SERVER_ADDRESS +'/api/Projects/PROJECTS'
+		});
+		// end of calls to get the data
+
+		axios.all([examplesPromise,publicProjectsPromise]).then(axios.spread((examples,projects)=>{
+				console.log('Data received from server',projects.data.length)
+		// TODO cache results and serve from cache
+				res.render('index.pug', {examples: examples.data, projects: projects.data });
+		})).catch((err)=>{
+		//handle errors
+				console.log('Failed to get projects data from netsblox server.',err)
+				res.status(500).send();
+		})
+
+});
 
 
-// Give them the SPA base page
-app.get('*', (req, res) => {
-    // get the exmaples and public projects data
-    // let serverAdr = 'https://editor.netsblox.org';
+app.get('/tutorials*', (req,res) => {
+	res.render('tutorials.pug',{})
+});
 
-    // get examples data
-    let examplesPromise = axios({
-      	url: serverAdr + '/api/Examples/EXAMPLES?metadata=true',
-      	method: 'get',
-    });
-
-    // get projects data
-    let publicProjectsPromise = axios({
-      method: 'GET',
-      url: serverAdr +'/api/Projects/PROJECTS'
-    });
-
-    // check if the user is logged in and grab data
-    
-    // let cookies = cookieParser.JSONCookies(req.cookies);
-    // console.log('Cookies: ', cookies);
-    // if (req.cookies.hasOwnProperty('netsblox-cookie')){
-    //     console.log('user seems to be logged in');
-    //     console.log(cookies['netsblox-cookie']);
-
-    // }
-    // let userProjects = axios({
-    //     url: serverAdr + '/api//getProjectList?format=json',
-    //     method: 'GET',
-    //     withCredentials: true
-    //     // crossDomain: true
-    // })
-    
-    // userProjects.catch(err => {
-    //     console.log(err)
-    // })
-
-
-
-    // end of calls to get the data
-
-    axios.all([examplesPromise,publicProjectsPromise]).then(axios.spread((examples,projects)=>{
-        console.log('Data received from server',projects.data.length)
-    // TODO cache results and serve from cache
-        res.render('index.pug', {examples: examples.data, projects: projects.data });
-    })).catch((err)=>{
-    //handle errors
-        console.log('Failed to get projects data from netsblox server.',err)
-        res.status(500).send();
-    })
-
-    });
-
+app.get('*', (req,res)=>{
+	res.status(404).send('Page not found. Go back to <a href="/">Home Page</a>. If you believe there is a mistake, please let us know at <a href="https://facebook.com/netsblox"> our facebook page</a>.');
+})
 
 /**********************************************************************************************************/
 
 // Run the server itself
-let server = app.listen(port, () => {
-    console.log('Example app listening on ' + server.address().port);
+let server = app.listen(PORT, () => {
+		console.log('Example app listening on ' + server.address().port);
 });
