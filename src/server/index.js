e6d5cb1fc7ce1b378ef774a22d1a8f09658f3787
Hamid Zare <hamidzr@users.noreply.github.com>
2019-01-11 16:04:22 -0600

    caching headers fixes #20 (#102)
    * public caching header for static resources
    
    * cache basic pug renders for an hour
    
    * caching header for the index page

--- src/server/index.js
+++ src/server/index.js
@@ -1,6 +1,7 @@
 'use strict';
 const path      = require('path'),
     express       = require('express'),
+    serveStatic = require('serve-static'),
     bodyParser    = require('body-parser'),
     axios         = require('axios'),
     http          = require('http'),
@@ -20,8 +21,19 @@ let app = express();
 app.use(cookieParser());
 // Setup pipeline logging
 if (NODE_ENV !== 'test') app.use(logger('dev'));
+
 // Setup pipeline support for static pages
-app.use(express.static(path.join(__dirname, '../../public')));
+// app.use(express.static(path.join(__dirname, '../../public')));
+app.use(serveStatic(path.join(__dirname, '../../public'), {
+    maxAge: '1h',
+    setHeaders: setCacheControl,
+}));
+
+function setCacheControl(res, path) {
+    res.setHeader('Cache-Control', 'public, max-age=3600');
+}
+
+
 // Setup pipeline support for server-side templates
 app.engine('pug', require('pug').__express);
 app.set('views', path.join(__dirname, 'views'));
@@ -52,13 +64,18 @@ let getExamples = memoize(() => {
 }, {promise: true, maxAge: 86400 });
 
 app.get('/', (req, res) => {
+
+    // set caching headers
+    res.set({
+        'Cache-Control': 'private, max-age=3600',
+    })
+
     // get the examples and public projects data
     // get examples data
     let examplesPromise = getExamples();
     // get projects data
     let publicProjectsPromise = getPublicProjects();
     // end of calls to get the data
-
 
     axios.all([examplesPromise,publicProjectsPromise]).then(axios.spread((examples,projects)=>{
         log.debug('Data received from server',projects.data.length);
@@ -73,17 +90,25 @@ app.get('/', (req, res) => {
 
 });
 
-app.get('/tutorials*', (req,res) => res.render('tutorials.pug',{}));
-app.get('/help', (req,res) => res.render('help.pug',{}));
-app.get('/howtos', (req,res) => res.render('howtos.pug',{}));
-app.get('/quickstart', (req,res) => res.render('quickstart.pug',{}));
-app.get('/mobile', (req,res) => res.render('mobile.pug',{}));
+function renderView(res, path) {
+    res.set({
+        'Cache-Control': 'public, max-age=3600',
+    });
+    return res.render(path, {});
+};
 
-app.get('/eclipse', (req,res) => res.render('eclipse.pug',{}));
-app.get('/eclipse/help', (req, res) => res.render('eclipse-help.pug'));
-app.get('/csta18', (req,res) => res.render('csta18.pug',{}));
+app.get('/tutorials*', (req,res) => renderView(res, 'tutorials.pug'));
+app.get('/help', (req,res) => renderView(res, 'help.pug'));
+app.get('/howtos', (req,res) => renderView(res, 'howtos.pug'));
+app.get('/quickstart', (req,res) => renderView(res, 'quickstart.pug'));
+app.get('/mobile', (req,res) => renderView(res, 'mobile.pug'));
+app.get('/eclipse', (req,res) => renderView(res, 'eclipse.pug'));
+app.get('/eclipse/help', (req, res) => renderView(res, 'eclipse-help.pug'));
+app.get('/csta18', (req,res) => renderView(res, 'csta18.pug'));
+
 app.get('/privacy.html', (req, res) => res.redirect(SERVER_ADDRESS + '/privacy.html'));
 app.get('/emailus', (req, res) => res.redirect('mailto:akos.ledeczi@vanderbilt.edu'));
+
 app.get('*', (req,res)=>{
     res.status(404).send('Page not found. Go back to <a href="/">Home Page</a>. If you believe there is a mistake, please let us know at <a href="https://facebook.com/netsblox"> our facebook page</a>.');
 });
